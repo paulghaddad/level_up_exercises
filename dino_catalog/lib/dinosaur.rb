beea36a77f31e029a797b39d9e17170b2b33ff3e
paulh16 <paulh16@gmail.com>
2014-11-06 16:15:51 -0600

    Refactor dinosaur.rb for arrowhead programming

--- dino_catalog/lib/dinosaur.rb
+++ dino_catalog/lib/dinosaur.rb
@@ -27,8 +27,8 @@ class Dinosaur
   end
 
   def to_hash
-    hash = {}
-    instance_variables.each { |var| hash[var.to_s.delete("@")] = instance_variable_get(var) }
-    hash
+    instance_variables.inject({}) do |variable|
+      hash[variable.to_s.delete("@")] = instance_variable_get(variable)
+    end
   end
 end
