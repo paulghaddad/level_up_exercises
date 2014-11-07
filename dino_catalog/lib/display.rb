f0b7ac129fae91ec786d1f1fdb7f67ec569b356e
paulh16 <paulh16@gmail.com>
2014-11-07 10:00:03 -0600

    Short method/lines of code for display.rb

--- dino_catalog/lib/display.rb
+++ dino_catalog/lib/display.rb
@@ -4,18 +4,16 @@ module Display
   HEADER_NAMES = ["Name", "Period", "Continent", "Diet", "Weight (lbs)", "Walking", "Description"]
 
   def print_dinosaur_set(set)
-    puts "\n\n"
     print_header
-    puts ''
     set.each do |entry|
-      print print_attribute(entry.name, PADDING)
-      print print_attribute(entry.period, PADDING)
-      print print_attribute(entry.continent, PADDING)
-      print print_attribute(entry.diet, PADDING)
-      print print_attribute(entry.weight_in_lbs, PADDING)
-      print print_attribute(entry.walking, PADDING)
-      print print_attribute(entry.description, PADDING)
-      print "\n"
+      print_attribute(entry.name, PADDING)
+      print_attribute(entry.period, PADDING)
+      print_attribute(entry.continent, PADDING)
+      print_attribute(entry.diet, PADDING)
+      print_attribute(entry.weight_in_lbs, PADDING)
+      print_attribute(entry.walking, PADDING)
+      print_attribute(entry.description, PADDING)
+      puts
     end
   end
 
@@ -31,14 +29,14 @@ module Display
       print "Walking".center(PADDING) if entry.walking
       print "Description".center(PADDING) if entry.description
       puts ''
-      print print_attribute(entry.name, PADDING) if entry.name
-      print print_attribute(entry.period, PADDING) if entry.period
-      print print_attribute(entry.continent, PADDING) if entry.continent
-      print print_attribute(entry.diet, PADDING) if entry.diet
-      print print_attribute(entry.weight_in_lbs, PADDING) if entry.weight_in_lbs
-      print print_attribute(entry.walking, PADDING) if entry.walking
-      print print_attribute(entry.description, PADDING) if entry.description
-      print "\n"
+      print_attribute(entry.name, PADDING) if entry.name
+      print_attribute(entry.period, PADDING) if entry.period
+      print_attribute(entry.continent, PADDING) if entry.continent
+      print_attribute(entry.diet, PADDING) if entry.diet
+      print_attribute(entry.weight_in_lbs, PADDING) if entry.weight_in_lbs
+      print_attribute(entry.walking, PADDING) if entry.walking
+      print_attribute(entry.description, PADDING) if entry.description
+      puts
     else
       puts "\nSorry, but I did not recognize that dinosaur name.\n"
     end
@@ -46,13 +44,15 @@ module Display
 
   def print_attribute(attribute, padding)
     if attribute
-      attribute.center(padding)
+      print attribute.center(padding)
     else
-      "---".center(padding)
+      print "---".center(padding)
     end
   end
 
   def print_header
+    puts "\n\n"
     HEADER_NAMES.each { |header| print header.center(PADDING) }
+    puts ''
   end
 end
