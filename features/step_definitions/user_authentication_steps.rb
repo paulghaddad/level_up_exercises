b56610ae88cff57331254fd4a7318276c911e4d6
paulh16 <paulh16@gmail.com>
2014-12-22 12:23:47 -0600

    Move over to Devise for Authentication

--- features/step_definitions/user_authentication_steps.rb
+++ features/step_definitions/user_authentication_steps.rb
@@ -6,7 +6,7 @@ When(/^I log in to my account$/) do
   visit signin_path
   fill_in("Email", with: "user@example.com")
   fill_in("Password", with: "secret")
-  click_button("Sign In")
+  click_button("Login")
 end
 
 Then(/^I should see a message confirming I am logged in$/) do
