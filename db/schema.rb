856b73a65c193ee4dcd0379b972fa93d078b94c7
paulh16 <paulh16@gmail.com>
2014-12-22 13:09:23 -0600

    Add first and last names to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141222185909) do
+ActiveRecord::Schema.define(version: 20141222190847) do
 
   create_table "artists", force: true do |t|
     t.string   "first_name"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 20141222185909) do
     t.datetime "confirmation_sent_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "Create new user" do
+
+    it "validates a new valid user" do
+      user = User.new
+      user.first_name = "Paul"
+      user.last_name = "Haddad"
+      user.email = "paul@example.com"
+      user.password = "secret"
+      user.password_confirmation = "secret"
+
+      expect(user).to be_valid
+    end
+
+    it "does not validate a new invalid user" do
+      user = User.new
+      user.first_name = ""
+      user.last_name = ""
+      user.email = "haddad"
+      user.password = ""
+      user.password_confirmation = ""
+
+      user.valid?
+
+      expect(user).not_to be_valid
+    end
+
+    it "does not validate a user with an invalid email address" do
+      user = User.new
+      user.first_name = "Paul"
+      user.last_name = "Haddad"
+      user.email = "paul"
+      user.password = "secret"
+      user.password_confirmation = "secret"
+
+      expect(user).not_to be_valid
+    end
+
+    describe "validations" do
+
+      it { should validate_presence_of(:first_name) }
+      it { should validate_presence_of(:last_name) }
+      it { should validate_presence_of(:email) }
+      it { should validate_uniqueness_of(:email) }
+      it { should have_secure_password }
+      it { should ensure_length_of(:password) }
+    end
+  end
+
+  describe "User methods" do
+
+    describe "#full_name" do
+      let(:user) { FactoryGirl.create(:user, first_name: "paul", last_name: "haddad") }
+
+      it "will concatanate the first and last names" do
+        full_name = user.full_name
+
+        expect(full_name).to eq("Paul Haddad")
+      end
+    end
+  end
 end
